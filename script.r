0f0e1e0c352356253c42b6c14c9b0c8d4c1d99ea
Marc <marc.f.dupuis@gmail.com>
2013-11-11 18:37:44 -0500

    Update

--- script.r
+++ script.r
@@ -1,18 +1,8 @@
 library("ggplot2")
 graphics.off()
 
-setwd('/Users/mfdupuis/Documents/Kaggle/Python/Flight-Data-Project/Flight Data/')
+setwd('/Users/mfdupuis/Documents/Kaggle/Python/Flight-Data-Project/')
 
-df <- read.csv('output13.csv')
+data <- read.csv('byCarrier.csv')
 
-df$DEP_DEL15[is.na(df$DEP_DEL15)] <- 0
-delays_by_carrier <- data.frame(tapply(df$DEP_DEL15, df$UNIQUE_CARRIER, sum))
-delays_by_carrier <- cbind(row.names(delays_by_carrier), delays_by_carrier)
-colnames(delays_by_carrier) <- c("Carrier","NumDelays")
-rownames(delays_by_carrier) <- c(array(1:nrow(delays_by_carrier)))
 
-delays_by_carrier <- delays_by_carrier[order(delays_by_carrier$NumDelays),]
-rownames(delays_by_carrier) <- c(array(1:nrow(delays_by_carrier)))
-
-c <- ggplot(delays_by_carrier, aes(Carrier,NumDelays))
-c + geom_bar(stat = "identity")
